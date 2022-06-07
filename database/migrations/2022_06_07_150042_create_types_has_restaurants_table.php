4250fafe7b4c2d08a5223ffd3bf4f1866b378dd6
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-07 18:17:09 +0300

    initial commit

---
+++ database/migrations/2022_06_07_150042_create_types_has_restaurants_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('types_has_restaurants', function (Blueprint $table) {
+            $table->Integer('types_id');
+            $table->Integer('restaurants_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('types_has_restaurants');
+    }
+};
