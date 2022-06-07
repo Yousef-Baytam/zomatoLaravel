359deca0a9a6a2af2e1ed60a49a68b17d41a60ad
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-07 18:38:47 +0300

    Adjusted users mnigration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile_pic');
+            $table->integer('user_type_id');
+            $table->integer('city_id');
+            $table->string('user_status');
+            $table->date('dob');
             $table->rememberToken();
             $table->timestamps();
         });
