cac86ba90246d4ea93a6c0727649c3425ece3a53
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-07 20:13:33 +0300

    Added a addReview Function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,20 @@ use App\Models\Review;
 
 class UserController extends Controller
 {
+    public function addReview(Request $request)
+    {
+        $review = new Review;
+        $review->rating = $request->rating;
+        $review->review = $request->review;
+        $review->user_id = $request->user_id;
+        $review->restaurant_id = $request->restaurant_id;
+        $review->status = $request->status;
+        $review->save();
+
+        return response()->json([
+            "status" => "Success"
+        ], 200);
+    }
 
     public function addUser(Request $request)
     {
