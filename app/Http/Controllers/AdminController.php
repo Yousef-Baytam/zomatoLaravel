3d4af5bfcc3cc29b6bfacd8f5c62dad91c470107
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-07 19:24:48 +0300

    Added a route and a create restro function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function addRestro(Request $request)
+    {
+        $restro = new Restaurant();
+        $restro->name = $request->name;
+        $restro->description = $request->description;
+        $restro->image = $request->image;
+        $restro->status = $request->status;
+        $restro->city_id = $request->city_id;
+        $restro->save();
+
+        return response()->json([
+            "status" => "Success"
+        ], 200);
+    }
 }
